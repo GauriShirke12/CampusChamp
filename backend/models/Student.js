b8e1f7105a6739ad8741fba905550229f2ca9940
Gauri Shirke <gaurishirke.2005@gmail.com>
2025-05-31 20:50:24 +0530

    add-file

---
+++ backend/models/Student.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const studentSchema = new mongoose.Schema({
+  collegeId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  collegeName: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+}, { timestamps: true });
+
+// 🔒 Hash password before saving
+studentSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// ✅ Method to compare password for login
+studentSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const Student = mongoose.model("Student", studentSchema);
+module.exports = Student;

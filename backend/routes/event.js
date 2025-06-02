c0b89cb889cc517d4ebbd753a8bb7c90310aeb0a
Gauri Shirke <gaurishirke.2005@gmail.com>
2025-06-02 09:57:27 +0530

    modified file

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -1,19 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { createEvent } = require("../controllers/eventController");
 const { protect } = require("../middleware/authMiddleware");
-const {
-  createEvent,
-  rsvpEvent,
-  getLeaderboard
-} = require("../controllers/eventController");
 
-// Route to create a new event
 router.post("/create", protect, createEvent);
-
-// RSVP route handled in controller
-router.post("/rsvp/:eventId", protect, rsvpEvent);
-
-// Leaderboard route
-router.get("/leaderboard", protect, getLeaderboard);
 
 module.exports = router;

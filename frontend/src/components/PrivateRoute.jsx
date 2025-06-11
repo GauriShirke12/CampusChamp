58cebbebcab1ea171106730343c99a502e03f388
Gauri Shirke <gaurishirke.2005@gmail.com>
2025-06-11 23:55:15 +0530

    Update PrivateRoute.jsx

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -6,13 +6,7 @@ const PrivateRoute = ({ children }) => {
   const { user } = useAuth();
   const location = useLocation();
 
-  // If user is authenticated, allow access
-  if (user) {
-    return children;
-  }
-
-  // If not authenticated, redirect to login with the original path saved
-  return <Navigate to="/login" replace state={{ from: location }} />;
+  return user ? children : <Navigate to="/login" replace state={{ from: location }} />;
 };
 
 export default PrivateRoute;

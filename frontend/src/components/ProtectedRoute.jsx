94e765ca63fe2f3c57a155eb59ce51d1587982d8
Gauri Shirke <gaurishirke.2005@gmail.com>
2025-06-12 18:50:14 +0530

    Create ProtectedRoute.jsx

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+// components/ProtectedRoute.jsx
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth();
+  const location = useLocation();
+
+  return user ? children : <Navigate to="/login" replace state={{ from: location }} />;
+};
+
+export default ProtectedRoute;

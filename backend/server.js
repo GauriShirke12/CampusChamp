aa9640577e96a21756b87a8588b6b6acf986636d
Gauri Shirke <gaurishirke.2005@gmail.com>
2025-06-15 00:53:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,30 @@ app.use((req, res) => {
   res.status(404).json({ message: "API route not found" });
 });
 
+
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const http = require("http");
+const socketIo = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// Global socket connection
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+// Export io to use in controllers
+module.exports.io = io;
+
